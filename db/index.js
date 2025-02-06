d147b94de9a25a1a7498e88c172757da6fb69d86
Huanye Zhu <zhuzhenghuanye@gmail.com>
2025-02-06 23:50:32 +0100

    test

--- db/index.js
+++ db/index.js
@@ -5,12 +5,12 @@ const db = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl:{
     rejectUnauthorized: false
-  }
-// user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
+  },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 db.connect()

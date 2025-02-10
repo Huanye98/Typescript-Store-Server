86ddf146e2921b0eb5a14d2cde0dc81ed60cd77c
Huanye Zhu <zhuzhenghuanye@gmail.com>
2025-02-10 12:12:00 +0100

    fixed cloudinary flow

--- API/controllers/cloudinary.controller.js
+++ API/controllers/cloudinary.controller.js
@@ -1,9 +1,7 @@
 const uploader = require("../middlewares/cloudinary.middlewares");
 
 const uploadImage = (req, res, next) => {
-  uploader.single("image")
     if (!req.file) {
-      // this will happend if cloudinary rejects the image for any reason
       res.status(400).json({
         errorMessage:
           "There was a problem uploading the image. Check image format and size.",
--- API/routes/cloudinary.routes.js
+++ API/routes/cloudinary.routes.js
@@ -2,23 +2,6 @@ const express = require("express")
 const router = express.Router()
 const cloudinaryController = require("../controllers/cloudinary.controller")
 const uploader = require("../middlewares/cloudinary.middlewares")
-// POST "/api/upload"
-// router.post("/", cloudinaryController.uploadImage );
 
-router.post("/", uploader.single("image"), (req, res, next) => {
-    // console.log("file is: ", req.file);
-  
-    if (!req.file) {
-      // this will happend if cloudinary rejects the image for any reason
-      res.status(400).json({
-        errorMessage: "There was a problem uploading the image. Check image format and size."
-      })
-      return;
-    }
-  
-    // get the URL of the uploaded file and send it as a response.
-    // 'imageUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend (response.data.imageUrl)
-  
-    res.json({ imageUrl: req.file.path });
-  });
+router.post("/", uploader.single("image"), cloudinaryController.uploadImage);
 module.exports = router;

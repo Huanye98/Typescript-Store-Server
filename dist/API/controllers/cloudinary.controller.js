10731f0c6b503efc4691022f604993cc1120f00f
Huanye Zhu <zhuzhenghuanye@gmail.com>
2025-10-10 00:07:45 +0200

    ts compile and multer ignore

--- dist/API/controllers/cloudinary.controller.js
+++ dist/API/controllers/cloudinary.controller.js
@@ -1,14 +1,13 @@
+"use strict";
+//@ts-nocheck
 const uploader = require("../middlewares/cloudinary.middlewares");
-
 const uploadImage = (req, res, next) => {
     if (!req.file) {
-      res.status(400).json({
-        errorMessage:
-          "There was a problem uploading the image. Check image format and size.",
-      });
-      return;
+        res.status(400).json({
+            errorMessage: "There was a problem uploading the image. Check image format and size.",
+        });
+        return;
     }
     res.json({ imageUrl: req.file.path });
 };
-
 module.exports = { uploadImage };
--- dist/API/middlewares/cloudinary.middlewares.js
+++ dist/API/middlewares/cloudinary.middlewares.js
@@ -1,4 +1,5 @@
 "use strict";
+//@ts-nocheck
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
